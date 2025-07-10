d2b189484bc3ce2c91c43fd6eb436d41d52cf59a
Tib <NotAn@ema.il>
2025-07-10 15:26:57 +0200

    ready_to_run

---
+++ experiments/fno2d_R/params_generator.py
@@ -0,0 +1,23 @@
+import itertools
+import os
+
+
+params_path = "fno2d_R/params"
+
+n_layers = [4, 8, 16]
+hidden_channels = [16, 32, 64]
+n_modes = [32, 64, 128]
+epochs = 1000
+
+if os.path.exists(params_path):
+    os.remove(params_path)
+
+
+with open(params_path, "a") as f:
+    for _ in range(10):
+        for nl, hc, nm in itertools.product(n_layers, hidden_channels, n_modes):
+
+            if nm == 128 and hc == 64:
+                continue
+
+            f.write(f"-l {nl} -c {hc} -m {nm} -e {epochs}\n")
