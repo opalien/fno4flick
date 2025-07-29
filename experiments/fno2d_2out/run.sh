6969020f81ef72f02b512f8213981d6fe6c91e77
Tib <NotAn@ema.il>
2025-07-29 12:23:09 +0200

    process.txt print

--- experiments/fno2d_2out/run.sh
+++ experiments/fno2d_2out/run.sh
@@ -17,7 +17,7 @@
 
 BATCH_HIST="batch.txt"
 
-CMD=$"srun python -m experiments.fno2d_2out.main -l 4 -m 64 -c 64 -e 200 -d examples/no_limit/Nt=1000_Nr=200/train/ -n no_limit_2out_Nt=1000_Nr=200_without_batch_hist_problem -b 16 -r True"
+CMD=$"srun python -m experiments.fno2d_2out.main -l 4 -m 64 -c 64 -e 200 -d examples/no_limit/Nt=1000_Nr=200/train/ -n no_limit_2out_Nt=1000_Nr=200_without_batch_hist_problem_processing_print -b 16 -r True >> processing.txt"
 
 echo "start"
 ml python/3.12
